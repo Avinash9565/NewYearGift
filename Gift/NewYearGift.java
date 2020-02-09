461e2dfe0f52adfbff65eacaf3f61b0d9e36b0eb
Avinash9565 <41907138+Avinash9565@users.noreply.github.com>
2020-02-09 14:54:57 +0530

    Update NewYearGift.java

--- Gift/NewYearGift.java
+++ Gift/NewYearGift.java
@@ -19,21 +19,63 @@ public class NewYearGift{
                     o=sc.nextInt();
                     switch(o){
                         case 1:
+                            System.out.println("Enter no of Desserts Kazuburfi\n");
+                            s_sweet=sc.nextfloat();
+                            Kazuburfi kz=new Kazuburfi("kazu",s_sweet,10);
+                            s_cost=Kz.cost();
+                            a.addSweets(s_cost,"Kazu");
+                            break
 
                         case 2:
+                            System.out.println("Enter no of Desserts Mysorepak\n");
+                            s_sweet=sc.nextfloat();
+                            Mysorepak kz=new Mysorepak("Mysore",s_sweet,10);
+                            s_cost=Kz.cost();
+                            a.addSweets(s_cost,"Mysore");
+                            break
 
                         case 3:
+                            
+                            System.out.println("Enter no of Desserts jalebi\n");
+                            s_sweet=sc.nextfloat();
+                            Jalebi jb=new Jalebi("jalebi",s_sweet,10);
+                            s_cost=jb.cost();
+                            a.addSweets(s_cost,"jalebi");
+                            break
                     }
                 case 2:
                     System.out.println("Enter your choice\n1.Cadbury\n2.Dairymilk\n3.eclairs");
                     o=sc.nextInt();
                     switch(o){
                         case 1:
+                            
+                            System.out.println("Enter no of cadbury you want\n");
+                            no_of_chocolates=s.nextInt();
+                            Cadbury cd=new Cadbury("Cadbury",no_of_chocolates,40);
+                            c_cost=cd.cost();
+                            a.addChocolates(c_cost);
+                            break;
                         case 2:
+                            
+                                System.out.println("Enter no of Dairymilk you want\n");
+                                no_of_chocolates=s.nextInt();
+                                Dairymilk dm=new Dairymilk("Cadbury",no_of_chocolates,40);
+                                c_cost=dm.cost();
+                                a.addChocolates(c_cost);
+                                break;
                         case 3:
+                            
+                                System.out.println("Enter no of eclairs you want\n");
+                                no_of_chocolates=s.nextInt();
+                                Eclairs ec=new Eclairs("Cadbury",no_of_chocolates,40);
+                                c_cost=ec.cost();
+                                a.addChocolates(c_cost);
+                                break;
                     }
                 case 3:
+                    a.sort()
                 case 4:
+                    a.display()
                 case 5:
                     break
             }
